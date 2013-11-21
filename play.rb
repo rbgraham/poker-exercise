bc952b7a6e7327131d7fc521a5afc7822c94bf47
Cory Buecker <cory.buecker@oracle.com>
2013-11-21 12:39:03 -0600

    interview question 3

--- play.rb
+++ play.rb
@@ -1,11 +1,3 @@
 $: << "./lib"
 
 require "game"
-
-game = Game.new
-game.deal
-game.select_best_hands
-game.players.each do |player|
-  print "#{player.name} has a #{player.hand.hand_name} (#{player.hand.cards.join(", ")})\n"
-end
-print "\nThe winner is #{game.winner.name} with a #{game.winner.hand.hand_name}\n"
